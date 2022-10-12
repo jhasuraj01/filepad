baa8e5e1f7fe6cd015d73ddc06dcfdafe02e6f00
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-10-12 19:35:28 +0000

    basic editor ready

--- src/components/EditorArea/index.tsx
+++ src/components/EditorArea/index.tsx
@@ -1,9 +1,15 @@
 import style from './index.module.scss'
+import { useEffect, useRef } from 'react'
 
 export function EditorArea() {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const element: HTMLDivElement = ref.current!
+    element.focus()
+  }, [ref])
+
   return (
-    <div className={style.container}>
-      Editor Area
-    </div>
+    <div ref={ref} className={style.container} contentEditable></div>
   )
 }
